f24f3850a15fe857578ef370e5524b4d603588fa
Kunal Marwaha <marwahaha@berkeley.edu>
2018-03-27 20:35:26 -0700

    Add eslint

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-'use strict';
-var moment = require('moment');
-var _ = require('lodash');
-const fs = require('fs');
+"use strict";
+var moment = require("moment");
+var _ = require("lodash");
+var fs = require("fs");
 
 // Constants
-var DIRECTORY = process.argv[1].substring(0, process.argv[1].lastIndexOf('/') + 1);
+var DIRECTORY = process.argv[1].substring(0, process.argv[1].lastIndexOf("/") + 1);
 var FRIENDS_PATH = DIRECTORY + "data/friends.json";
 var EVENTS_PATH = DIRECTORY + "data/events.json";
 var CONFIG_PATH = DIRECTORY + "config.json";
@@ -12,7 +12,7 @@ var CONFIG_PATH = DIRECTORY + "config.json";
 var DATE_STORAGE_FORMAT = "YYYY-MM-DD";
 var DATE_PARSE_FORMAT = "YYYY-MM-DD";
 var DATE_DISPLAY_FORMAT = "YYYY-MM-DD";
-var TODAY = moment().startOf('day');
+var TODAY = moment().startOf("day");
 var DEFAULT_NUM_DAYS = 10;
 
 var config = loadConfigData();
@@ -41,9 +41,8 @@ function writeEventsData(events) {
 function stringify(jsonObject) {
   if (config.prettyPrintJson) {
     return JSON.stringify(jsonObject, null, 2);
-  } else {
-    return JSON.stringify(jsonObject);
   }
+  return JSON.stringify(jsonObject);
 }
 
 function loadConfigData() {
@@ -75,24 +74,24 @@ function listFriends() {
   var events = loadEventsData();
   var friends = loadFriendsData();
 
-  var eventsByFriend = _.groupBy(events, 'name');
+  var eventsByFriend = _.groupBy(events, "name");
   var nextEventByFriend = _.map(friends, friend => {
-    var lastEvent = _.sortBy(eventsByFriend[friend.name], 'date').reverse()[0];
+    var lastEvent = _.sortBy(eventsByFriend[friend.name], "date").reverse()[0];
     return {
       name: friend.name,
-      date: lastEvent ? moment(lastEvent.date).add(friend.interval, 'days').format(DATE_DISPLAY_FORMAT) : NEW_INDICATOR
+      date: lastEvent ? moment(lastEvent.date).add(friend.interval, "days").format(DATE_DISPLAY_FORMAT) : NEW_INDICATOR
     };
   });
-  _.sortBy(nextEventByFriend, 'date').forEach(friend => {
-    console.log(friend.date + '  ' + friend.name);
+  _.sortBy(nextEventByFriend, "date").forEach(friend => {
+    console.log(friend.date + "  " + friend.name);
   });
 }
 
 function addFriend(name, days) {
   var friends = loadFriendsData();
   var myFriend = getFriendByName(friends, name)[0];
-  if (!!myFriend) {
-    fail("Friend " + name + " already exists")
+  if (myFriend) {
+    fail("Friend " + name + " already exists");
   }
   var numDays = days !== undefined ? convertDaysToNumber(days) : DEFAULT_NUM_DAYS;
   friends.push({"name": name, "interval": numDays});
@@ -104,7 +103,7 @@ function editFriend(name, days) {
   var friends = loadFriendsData();
   var myFriend = getFriendByName(friends, name)[0];
   if (!myFriend) {
-    fail("Friend " + name + " does not exist")
+    fail("Friend " + name + " does not exist");
   }
   var numDays = convertDaysToNumber(days);
   var oldNumDays = myFriend.interval;
@@ -140,10 +139,10 @@ function addEvent(friendName, date, memo) {
  */
 function parseDate(s) {
   var date;
-  if (s === 'today') {
+  if (s === "today") {
     date = moment();
-  } else if (s === 'yesterday' || s === 'yday') {
-    date = moment().subtract(1, 'days');
+  } else if (s === "yesterday" || s === "yday") {
+    date = moment().subtract(1, "days");
   } else if (parseWeekday(s)) {
     date = parseWeekday(s);
   } else {
@@ -152,21 +151,21 @@ function parseDate(s) {
       fail("Invalid date (specified '" + s + "')");
     }
   }
-  return date.startOf('day');
+  return date.startOf("day");
 }
 
 function parseWeekday(s) {
   const weekdays = [
-    ['sunday',    'sun'],
-    ['monday',    'mon'],
-    ['tuesday',   'tue'],
-    ['wednesday', 'wed'],
-    ['thursday',  'thu'],
-    ['friday',    'fri'],
-    ['saturday',  'sat'],
+    ["sunday",    "sun"],
+    ["monday",    "mon"],
+    ["tuesday",   "tue"],
+    ["wednesday", "wed"],
+    ["thursday",  "thu"],
+    ["friday",    "fri"],
+    ["saturday",  "sat"],
   ];
   for (let i = 1; i <= 7; i++) {
-    const d = moment().startOf('day').subtract(i, 'days');
+    const d = TODAY.subtract(i, "days");
     if (weekdays[d.day()].indexOf(s) !== -1) {
       return d;
     }
@@ -175,13 +174,13 @@ function parseWeekday(s) {
 }
 
 function showHelp() {
-  console.log("welcome to friendlog :-)")
-  console.log("   add [friend] [interval=10] " + "Adds [friend]. Events expected every [interval] days")
-  console.log("   list                       " + "Lists each friend and their next expected event")
-  console.log("   list [friend]              " + "View info about [friend]")
-  console.log("   edit [friend] [interval]   " + "Edits [friend]'s expected [interval]")
-  console.log("   hangout [f] [d] [m]        " + "Records event with [friend] on [date] with [memo]")
-  console.log("   history [?friend]          " + "See history of all friends or specific [friend]")
+  console.log("welcome to friendlog :-)");
+  console.log("   add [friend] [interval=10] " + "Adds [friend]. Events expected every [interval] days");
+  console.log("   list                       " + "Lists each friend and their next expected event");
+  console.log("   list [friend]              " + "View info about [friend]");
+  console.log("   edit [friend] [interval]   " + "Edits [friend]'s expected [interval]");
+  console.log("   hangout [f] [d] [m]        " + "Records event with [friend] on [date] with [memo]");
+  console.log("   history [?friend]          " + "See history of all friends or specific [friend]");
 }
 
 function fail(msg) {
@@ -190,14 +189,14 @@ function fail(msg) {
 
 function showHistory(friendName) {
   var events = loadEventsData();
-  var eventsByFriend = _.groupBy(events, 'name');
+  var eventsByFriend = _.groupBy(events, "name");
   Object.keys(eventsByFriend).sort().forEach(friend => {
     if (!friendName || friendName === friend) {
       prettyPrintFriendHeader(friend);
-      _.sortBy(eventsByFriend[friend], 'date').reverse().map(prettyPrintEvent);
+      _.sortBy(eventsByFriend[friend], "date").reverse().map(prettyPrintEvent);
       console.log();
     }
-  })
+  });
 }
 
 function prettyPrintFriendHeader(friend) {
@@ -206,7 +205,7 @@ function prettyPrintFriendHeader(friend) {
 }
 
 function prettyPrintEvent(event) {
-  console.log(event.date + '  ' + event.memo);
+  console.log(event.date + "  " + event.memo);
 }
 
 // Default behavior
@@ -217,21 +216,21 @@ function main() {
   var args = process.argv.slice(2);
   if (0 === args.length) {
     callDefault();
-  } else if (2 === args.length && 'add' === args[0]) {
+  } else if (2 === args.length && "add" === args[0]) {
     addFriend(args[1]);
-  } else if (3 === args.length && 'add' === args[0]) {
+  } else if (3 === args.length && "add" === args[0]) {
     addFriend(args[1], args[2]);
-  } else if (1 === args.length && 'list' === args[0]) {
+  } else if (1 === args.length && "list" === args[0]) {
     listFriends();
-  } else if (2 === args.length && 'list' === args[0]) {
+  } else if (2 === args.length && "list" === args[0]) {
     listFriend(args[1]);
-  } else if (3 === args.length && 'edit' === args[0]) {
+  } else if (3 === args.length && "edit" === args[0]) {
     editFriend(args[1], args[2]);
-  } else if (1 === args.length && 'history' === args[0]) {
+  } else if (1 === args.length && "history" === args[0]) {
     showHistory();
-    } else if (2 === args.length && 'history' === args[0]) {
+  } else if (2 === args.length && "history" === args[0]) {
     showHistory(args[1]);
-  } else if (4 === args.length && 'hangout' === args[0]) {
+  } else if (4 === args.length && "hangout" === args[0]) {
     addEvent(args[1], args[2], args[3]);
   } else {
     showHelp();
